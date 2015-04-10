8a5c4fd8e040482733ff186abc92fdeac2a25c59
TianyiChen <TianyiChen@users.noreply.github.com>
2015-04-10 21:48:21 +0800

    297MS -> 141MS

--- 1256.cpp
+++ 1256.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 char c[13];
 int main(){
-	int cs,t,len; cin >> cs; 
+	int cs, t, len; cin >> cs;
 	while (cs--){
-		cin >> c; len = strlen(c);
+		scanf("%s",c); len = strlen(c);
 		for (t = 0; t < len; t++)
-			if (c[t] <= 'Z')c[t] = (c[t] - 0x40) * 2; 
+			if (c[t] <= 'Z')c[t] = (c[t] - 0x40) * 2;
 			else c[t] = (c[t] - 0x60) * 2 + 1;
 			sort(c, c + len);
 			do{
 				for (t = 0; t < len; t++)
-					printf("%c", c[t] & 1 ? c[t] / 2 + 0x60 : c[t] / 2 + 0x40);
-				printf("\n");
+					putchar( c[t] & 1 ? c[t] / 2 + 0x60 : c[t] / 2 + 0x40);
+				putchar('\n');
 			} while (next_permutation(c, c + len));
 	}
-	return 0;}
+	return 0;
+}
